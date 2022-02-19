5811c969c7d4fa816ab9b238357b278086286903
Rodrigo Trivel <rodrigo@lexartlabs.xyz>
2022-02-19 10:54:18 -0300

    Compontente usuario

--- UserComponent/app.js
+++ UserComponent/app.js
@@ -1,4 +1,4 @@
-const API   = 'https://crudcrud.com/api/04b028d649494e2191b0f87733ebf552/'
+const API   = 'https://crudcrud.com/api/abf37e651d2d44f0b748360d44faf136/'
 const TIME_MODAL = 3000
 const MODAL_MSG  = "Esta seguro que quiere eliminar el usuario?"
 const app = new Vue({
